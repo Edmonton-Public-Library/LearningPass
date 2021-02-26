c1b7dc03c03a1fa31ba6f1a9547924f443858f91
Andrew Nisbet <andrew@dev-ils.com>
2021-02-25 21:36:34 -0500

    GetStreet() tested.

--- tests/helpers-test.js
+++ tests/helpers-test.js
@@ -32,34 +32,34 @@ test('andrew@example.com should return true', () => {
     assert.strictEqual(helpers.getEmail(possibleAddr), possibleAddr);
 });
 
-test('_@_._ should return true', () => {
+test('_@_._ should return ""', () => {
     let possibleAddr = '_@_._';
     assert.strictEqual(helpers.getEmail(possibleAddr), '');
 });
 
-test('a@b should return true', () => {
+test('a@b should return ""', () => {
     let possibleAddr = 'a@b';
     assert.strictEqual(helpers.getEmail(possibleAddr), '');
 });
 
-test('a@b@c should return true', () => {
+test('a@b@c should return ""', () => {
     let possibleAddr = 'a@b@c';
     assert.strictEqual(helpers.getEmail(possibleAddr), '');
 });
 
 // Tests for valid phone
 const phone = '780-242-5555';
-test('+1(780) 242-5555 should return true', () => {
+test('+1(780) 242-5555 should return '+phone, () => {
     let str = '+1(780) 242-5555';
     assert.strictEqual(helpers.getPhone(str), phone);
 });
 
-test('+1 780 242-5555 should return true', () => {
+test('+1 780 242-5555 should return '+phone, () => {
     let str = '+1 780 242-5555';
     assert.strictEqual(helpers.getPhone(str), phone);
 });
 
-test('780 242-5555 should return true', () => {
+test('780 242-5555 should return '+phone, () => {
     let str = '780 242-5555';
     assert.strictEqual(helpers.getPhone(str), phone);
 });
@@ -69,3 +69,85 @@ test('78O 242-5555 should return ""', () => {
     let phone = '';
     assert.strictEqual(helpers.getPhone(str), phone);
 });
+
+// Tests for valid Postal code.
+const pc = 'N2V2V4';
+test(`getPostalCode() should return ${pc}`, () => {
+    let str = 'N2V2V4';
+    assert.strictEqual(helpers.getPostalCode(str), pc);
+});
+
+test(`getPostalCode() should return ${pc}`, () => {
+    let str = 'N2V 2V4';
+    assert.strictEqual(helpers.getPostalCode(str), pc);
+});
+
+test(`getPostalCode() should return ${pc}`, () => {
+    let str = ' N2V 2V4 ';
+    assert.strictEqual(helpers.getPostalCode(str), pc);
+});
+
+test(`getPostalCode() should return ${pc}`, () => {
+    let str = ' n2V-2V4 ';
+    assert.strictEqual(helpers.getPostalCode(str), pc);
+});
+
+test(`getPostalCode() should return ${pc}`, () => {
+    let str = '(N2V-2V4)';
+    assert.strictEqual(helpers.getPostalCode(str), pc);
+});
+
+test("getPostalCode() should return ''", () => {
+    let str = 'h0h0ho';
+    assert.strictEqual(helpers.getPostalCode(str), '');
+});
+
+// Test for street.
+test("getStreet() should return '11811 74 Ave.'", () => {
+    let str = '11811 74 Ave.';
+    let street = '11811 74 Ave.';
+    assert.strictEqual(helpers.getStreet(str), street);
+});
+
+test("getStreet() should return '11811 74 Ave.'", () => {
+    let str = '11811 74 AVE.';
+    let street = '11811 74 Ave.';
+    assert.strictEqual(helpers.getStreet(str), street);
+});
+
+test("getStreet() should return '11811 74 Ave.'", () => {
+    let str = '11811-74 AVE.';
+    let street = '11811 74 Ave.';
+    assert.strictEqual(helpers.getStreet(str), street);
+});
+
+test("getStreet() should return '20 A B Street.'", () => {
+    let str = '20 a-b STREET.';
+    let street = '20 A B Street.';
+    assert.strictEqual(helpers.getStreet(str), street);
+});
+
+test("getStreet() should return '1277 Elgin Cres. Oakville Ontario'", () => {
+    let str = '1277 ElGIn CreS. OakVILLe oNTaRio';
+    let street = '1277 Elgin Cres. Oakville Ontario';
+    assert.strictEqual(helpers.getStreet(str), street);
+});
+
+test("getStreet() should return 'Stanley Milner Library, not 27 Calgary street'", () => {
+    let str = 'Stanley Milner Library, not 27 Calgary street';
+    let street = 'Stanley Milner Library, Not 27 Calgary Street';
+    assert.strictEqual(helpers.getStreet(str), street);
+});
+
+test("getStreet() should return 'Apt. 12 53-Rubbish Lane.'", () => {
+    let str = 'Apt. 12 53-Rubbish Lane.';
+    let street = 'Apt. 12 53 Rubbish Lane.';
+    assert.strictEqual(helpers.getStreet(str), street);
+});
+
+test("getStreet() should return '123 456 Street.'", () => {
+    let str = '-123 456 street.';
+    let street = '123 456 Street.';
+    assert.strictEqual(helpers.getStreet(str), street);
+});
+
